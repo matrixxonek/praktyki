91c7f3ec61f69957d853887367b4b93d6e8fbab6
matrixxonek <matrixxon@gmail.com>
2022-09-27 09:25:00 +0200

    odczyt i zapis

---
+++ odczyt_zapis/odczyt_zapis/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace odczyt_zapis
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string folder = "C:\\Users\\Admin\\Source\\Repos\\praktyki\\odczyt_zapis";
+            StreamWriter writer = new StreamWriter(folder + "\\zapis.txt");
+            StreamReader reader = new StreamReader(folder + "\\odczyt.txt");
+            writer.WriteLine("Nowy zapis");
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                writer.WriteLine(line);
+            }
+            writer.Close();
+            reader.Close();
+
+        }
+
+    }
+}
