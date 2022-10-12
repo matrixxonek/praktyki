9fd31a6049790c7467a769eafc9e08f949f79341
matrixxonek <matrixxon@gmail.com>
2022-10-12 14:00:45 +0200

    more assets

---
+++ GitLabTest/GitLabTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+using System.Threading.Tasks;
+HttpClient client = new HttpClient();
+async Task ProcessRepositories()
+{
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+    client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+    var stringTask = client.GetStringAsync("https://gitlab.com/matrixxonek/my-awesome-project");
+
+    var msg = await stringTask;
+    Console.Write(msg);
+}
+async Task Main(string[] args)
+{
+    Console.WriteLine("wiad");
+    await ProcessRepositories();
+}
---
+++ timer/timer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Timers;
+
+public class Example
+{
+    private static System.Timers.Timer aTimer;
+
+    public static void Main()
+    {
+        // Create a timer and set a two second interval.
+        aTimer = new System.Timers.Timer();
+        aTimer.Interval = 2000;
+
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimedEvent;
+
+        // Have the timer fire repeated events (true is the default)
+        aTimer.AutoReset = true;
+
+        // Start the timer
+        aTimer.Enabled = true;
+
+        Console.WriteLine("Press the Enter key to exit the program at any time... ");
+        Console.ReadLine();
+    }
+
+    private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+    {
+        Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+    }
+}
